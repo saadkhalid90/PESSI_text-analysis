9ee9be1c923dd560419d0a75761b9a19f0c54dc4
Saad Khalid <saadkhalid90@gmail.com>
2018-02-14 12:00:04 +0500

    Cosine distance for school matching

---
+++ compare.R
@@ -0,0 +1,75 @@
+## importing relevant libraries
+library(readr)
+library(dplyr)
+library(stringdist)
+
+## read in the files and selecting relevant features
+
+PESSI <- read.csv('PESSI_Multan.csv', encoding = 'UTF-8') %>% select(Unit.Name, Tehsil, Nature.of.Business)
+## converting factors to character variables
+PESSI <- data.frame(sapply(X = PESSI, FUN = as.character), stringsAsFactors=FALSE)
+
+str(PESSI)
+
+PVT_list <- read_csv('Pvt_schools_Multan_dir.csv') %>% select(SchoolName, TehsilName)
+
+PESSI$Unit.Name <- gsub(pattern = '[Mm]/( )?[Ss](.)?', replacement = "", x = PESSI$Unit.Name)
+
+
+PESSI$Unit.Name <- toupper(trimws(PESSI$Unit.Name, which = 'both'))
+PESSI <- data.frame(sapply(X = PESSI, FUN = function(x) return(toupper(trimws(x, which = 'both')))), stringsAsFactors=FALSE)
+
+
+logical <- grepl(pattern = "SCHOOL|ACAD|EDUCATOR|COLLEGE", x = PESSI$Unit.Name) | PESSI$Nature.of.Business == "SCHOOLS"
+
+sum(logical)
+PESSI_schools <- data.frame(PESSI[logical, ])
+
+PVT_list$TehsilName <- gsub(pattern = "MULTAN (.)+", replacement = "MULTAN", x = PVT_list$TehsilName)
+PVT_list$TehsilName <- gsub(pattern = " ABAD", replacement = "BAD", x = PVT_list$TehsilName)
+
+## checking if the tehsils in both datasets are the same:
+## adjusting indices as PESSI data also has blank Tehsil fields
+sort(unique(PVT_list$TehsilName)) == sort(unique(PESSI$Tehsil))[2:10]
+
+
+tehsils <- sort(unique(PVT_list$TehsilName))
+
+consol <- data.frame(matrix(vector(), 0, 6,
+                            dimnames=list(c(), c("PESSI_schools", "BM1", "BM2", "BM3", "BM4", "BM5"))),
+                     stringsAsFactors=F)
+
+
+for (i in tehsils){
+  print(i)
+  ## i <- "MULTAN"
+  ## subset based on tehsil
+  PESSI_schools_subset <- PESSI_schools %>% filter(Tehsil == i)
+  PVT_schools_list <- PVT_list %>% filter(TehsilName == i) 
+  
+
+  dist_matrix_q2 <- stringdistmatrix(a = PESSI_schools_subset$Unit.Name, b = PVT_schools_list$SchoolName, method = "cosine", q = 2)
+  dist_matrix_q3 <- stringdistmatrix(a = PESSI_schools_subset$Unit.Name, b = PVT_schools_list$SchoolName, method = "cosine", q = 3)
+  dist_matrix_q4 <- stringdistmatrix(a = PESSI_schools_subset$Unit.Name, b = PVT_schools_list$SchoolName, method = "cosine", q = 4)
+  
+  dist_matrix <- (dist_matrix_q2 + dist_matrix_q3 + dist_matrix_q4)/ 3
+  
+  min_5_idx <- apply(X = dist_matrix, MARGIN = 1, FUN = function(a){return(c(which(a == sort(a)[1]),
+                                                                             which(a == sort(a)[2]),
+                                                                             which(a == sort(a)[3]),
+                                                                             which(a == sort(a)[4]),
+                                                                             which(a == sort(a)[5]))[1:5])})
+  min_5_idx <- t(min_5_idx)
+  
+  test <- as.data.frame(matrix(PVT_schools_list$SchoolName[min_5_idx], dim(dist_matrix)[1], 5))
+  PESSI_schools_units <- PESSI_schools_subset$Unit.Name
+  comp_matrix <- cbind(PESSI_schools_units, test)
+  
+  ##print(consol)
+  print(comp_matrix)
+  
+  consol <- rbind(consol, comp_matrix)
+
+}
+
+
